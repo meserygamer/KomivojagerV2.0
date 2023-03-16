2f8cb3f4fbc4b968a711ab1765f8dd0e584b41fb
meserygamer <meserygamer@gmail.com>
2023-03-16 19:36:18 +0300

    Ver0.2

--- Class1.cs
+++ Class1.cs
@@ -8,24 +8,60 @@ namespace KomivojagerV2._0
 {
     internal class TreeNode //Элемент графа
     {
+        //public static uint[][] TableOfTransport; //Матрица путей
         int IDDotInside; //Значение точки внутри
         TreeNode? Predok; //Родитель
         TreeNode[]? Children; //Потомки
         int Path; //Путь до этого узла
         int level; //Уровень в иерархии
-        public TreeNode(int IDDotIside) //Конструктор для головы графа
+        public TreeNode(int IDDotIside, int[][] TableOfTransport) //Конструктор для головы графа
         {
             this.IDDotInside = IDDotIside;
             Path = 0;
             level = 0;
+            Console.WriteLine($"Я Корень,Узел№{IDDotInside}, Path:{Path}");
+            PoiskPotomkov(TableOfTransport);
         }
-        public void PoiskPotomkov(uint[][] TableOfTransport)
+        public TreeNode(int IDDotIside, TreeNode Parent, int[][] TableOfTransport) //Конструктор для потомков
         {
-
+            this.IDDotInside = IDDotIside;
+            Path = Parent.Path + TableOfTransport[Parent.IDDotInside - 1][IDDotInside - 1];
+            level = Parent.level + 1;
+            Predok = Parent;
+            Console.WriteLine($"Я узел, Узел№{IDDotInside}, Path:{Path}, Уровень:{level}");
+            PoiskPotomkov(TableOfTransport);
         }
-        private List<int>? ListOfNextDot(int[] VectorOfDot)
+        public void PoiskPotomkov(int[][] TableOfTransport) //Ищет и создаёт потомков
         {
-
+            List<int>? NextDot = ListOfNextDot(TableOfTransport[IDDotInside - 1]);
+            if(NextDot is not null)
+            {
+                Children = new TreeNode[NextDot.Count];
+                for(int i = 0; i < NextDot.Count; i++)
+                {
+                    Children[i] = new TreeNode(NextDot[i], this, TableOfTransport);
+                }
+            }
+        }
+        private List<int>? ListOfNextDot(int[] VectorOfDot) //Возвращает список потомков
+        {
+            List<int> ListOfMaybeDot, ListOfMaybeDotCopy; //Список возможных потомков (Он изменяется при работе), и его копия(Не изменяется, нужна чтобы проверить все точки и вычеркнуть из оригинала)
+            if(Min(VectorOfDot) is not null) //Если предполагаемые точки существуют, то идём дальше, иначе возвращаем null, так как граф закончен
+            {
+                ListOfMaybeDot = PoiskDotSMinMarsh((int)Min(VectorOfDot), VectorOfDot); //Составляем набор точек
+                ListOfMaybeDotCopy = new List<int>(ListOfMaybeDot);
+                foreach (var i in ListOfMaybeDotCopy) //Проходим по всем предполагаемым точкам
+                {
+                    if(ToBeDot(this, i)) //Если точка уже встречалась вычёркиваем
+                    {
+                        ListOfMaybeDot.Remove(i);
+                        VectorOfDot[(i - 1)] = 0;
+                    }
+                }
+                if (ListOfMaybeDot.Count == 0) return ListOfNextDot(VectorOfDot);
+                else return ListOfMaybeDot;
+            }
+            return null;
         }
         private List<int> PoiskDotSMinMarsh(int Min, int[] VectorOfDot) //Возвращает набор точек с минимальным маршрутом
         {
@@ -48,12 +84,24 @@ namespace KomivojagerV2._0
                 {
                     MinMarsh = VectorOfDot[i];
                 }
-                if(MinMarsh is not null && VectorOfDot[i] > MinMarsh)
+                if(MinMarsh is not null && VectorOfDot[i] != 0 && VectorOfDot[i] < MinMarsh)
                 {
                     MinMarsh = VectorOfDot[i];
                 }
             }
             return MinMarsh;
+        }
+        private bool ToBeDot(TreeNode Node, int ID)
+        {
+            if(Node.IDDotInside == ID)
+            {
+                return true;
+            }
+            if(Node.level == 0)
+            {
+                return false;
+            }
+            return ToBeDot(Node.Predok, ID);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,28 +4,33 @@
     {
         static void Main(string[] args)
         {
+            /*List<int> ints = new List<int>() {1,3};
+            ints.Remove(1);
+            ints.Remove(3);
+            Console.WriteLine($"Count:{ints.Count} Capacity:{ints.Capacity}");*/
             travelling_salesman_problem Vojager = new travelling_salesman_problem();
             Vojager.BodyOfMethod();
         }
     }
     class travelling_salesman_problem
     {
-        protected uint[][] TableOfTransport; //Таблица для хранения стоимостей переходов между вершинами
+        protected int[][] TableOfTransport; //Таблица для хранения стоимостей переходов между вершинами
         readonly int KolVersh;
         TreeNode[] Graphs;
         public travelling_salesman_problem()
         {
             Console.WriteLine("Введите количество вершин");
             KolVersh = Convert.ToInt32(Console.ReadLine());
-            TableOfTransport = new uint[KolVersh][];
+            TableOfTransport = new int[KolVersh][];
             ZapolnTranTable();
             Graphs = new TreeNode[KolVersh];
         }
         public void BodyOfMethod()
         {
+            //TreeNode.TableOfTransport = this.TableOfTransport;
             for (int i = 0; i < KolVersh; i++)
             {
-                Graphs[i] = new TreeNode(i+1);
+                Graphs[i] = new TreeNode(i+1, TableOfTransport);
             }
         }
         private void ZapolnTranTable() //Для заполнения транспортной таблицы
@@ -33,7 +38,7 @@
             for (int i = 0; i < KolVersh; i++)
             {
                 Console.WriteLine($"Введите строку №{i + 1}");
-                TableOfTransport[i] = Array.ConvertAll(Console.ReadLine().Split(" "), n => Convert.ToUInt32(n));
+                TableOfTransport[i] = Array.ConvertAll(Console.ReadLine().Split(" "), n => Convert.ToInt32(n));
             }
         }
     }
